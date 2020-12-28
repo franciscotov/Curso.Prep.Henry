8e493a879fa7383bb4043595d148119a8f4aee76
franciscotov <frank.ronaldo_17@hotmail.com>
2020-12-27 22:26:44 -0300

    homeworks V and VI completed

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,14 +8,30 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-
+  // Creamos la funcion constructora
+  function Usuario(opciones) {
+    //definimos las propiedades con los nombres indicados y los valores dados por el objetos opciones
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  }
+  // Definimos el método saludar en el prototipo de nuestra función constructora
+  Usuario.prototype.saludar = function() {
+    return 'Hola, mi nombre es ' + this.nombre;
+  };
+  // Retornamos la función construtora
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-
+  // Definimos el metodo saludar en el prototipo de la función constructora dada
+  Constructor.prototype.saludar = function() {
+    return "Hello World!";
+  }
 }
 
 function agregarStringInvertida() {
@@ -24,7 +40,15 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-
+  // Agregamos el método al porototipo String
+  String.prototype.reverse = function() {
+    // De la manera en que es llamado reverse, this seria asociado a al string por lo que este contiene el string
+    // en si, luego podemos separarlo por letra en un array con el metodo .split, lo invertimos con el uso de .reverse del proto
+    //type array y por último volver a unirlo en un string con el uso de .join
+    // otra forma sería hacer un cilo for para recorrer todo el string y concatenar en una variable cada letra
+    //desde la última a la primera
+    return this.split('').reverse().join('');
+  }
 }
 
 // No modificar nada debajo de esta línea
--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -2,23 +2,32 @@
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+  cb();
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+  // usamos la función reduce para sumar los números del array y pasamos el resultado a la función callback recibida
+  //pd : lo pasado a la función reduce es una función de flecha
+  cb(numeros.reduce((acc, i) => acc+i));
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  //iteramos por los elementos de array usando forEach
+  //sabiendo que cb es una función callback que es lo que necesita forEach la pasamos como parámetro
+  array.forEach(cb);
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  // usamos el método .map pasando nuestra función cb dada, esto nos devuelve un nuevo array 
+  return array.map(cb);
 }
 
 // No modificar nada debajo de esta línea
